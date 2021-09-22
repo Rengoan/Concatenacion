70aba533ec515019e3fa63b2ff8acfa475a884c6
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-22 11:57:21 +0200

    Pruebas de concatenacion

---
+++ Concatenacion/src/concatenacion/Concatenacion.java
@@ -0,0 +1,22 @@
+/*
+ * 
+ * Ejemplo concatenacion
+ * 
+ */
+package concatenacion;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Concatenacion {
+
+    public static void main(String[] args) {
+        var alumno = "Pedro";
+        var titulo = "Mecanico";
+        
+        var concat = titulo + " " + alumno;
+        System.out.println("concat = " + concat);
+    }
+    
+}
