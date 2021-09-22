50b393018cb2dcaf95120d733a74abd233f6157c
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-22 12:19:51 +0200

    Update Concatenacion.java

--- Concatenacion/src/concatenacion/Concatenacion.java
+++ Concatenacion/src/concatenacion/Concatenacion.java
@@ -14,9 +14,18 @@ public class Concatenacion {
     public static void main(String[] args) {
         var alumno = "Pedro";
         var titulo = "Mecanico";
-        
+
         var concat = titulo + " " + alumno;
         System.out.println("concat = " + concat);
+
+//        Con variables enteras
+        var valor1 = 1;
+        var valor2 = 2;
+        System.out.println(valor1 + valor2);
+        System.out.println(valor1 + valor2 + alumno);
+        System.out.println(alumno + valor1 + valor2);
+        System.out.println(alumno + (valor1 + valor2));
+
     }
-    
+
 }
